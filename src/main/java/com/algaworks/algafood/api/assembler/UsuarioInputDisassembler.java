4233c22d340cbec2e39ef971e52661ba4a8f1620
Bruno Caio Dias <brualfa@gmail.com>
2021-09-15 16:38:37 -0300

    Ajustes na aplicação - inclusao de anotacoes e repositorio

--- src/main/java/com/algaworks/algafood/api/assembler/UsuarioInputDisassembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/UsuarioInputDisassembler.java
@@ -2,10 +2,12 @@ package com.algaworks.algafood.api.assembler;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.model.input.UsuarioInput;
 import com.algaworks.algafood.domain.model.Usuario;
 
+@Component
 public class UsuarioInputDisassembler {
 
 	@Autowired
